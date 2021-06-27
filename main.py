7c17e231cc551324ada97f850a96493242b88346
tsiozos <38419287+tsiozos@users.noreply.github.com>
2021-06-27 10:37:14 +0300

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,122 +1,134 @@
+#
+# Button A: print mean total time
+# Button B: print statistics
+# total time counting
+
+# to apply to a different sensor just change the input
+def get_data_point():
+    global ll3
+    ll3 = input.light_level()
+    # print(str(ll))
+    return ll3
+
+def on_button_pressed_ab():
+    setup()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_a():
+    serial.write_string("mean light level=" + ("" + str(mea)) + "  stdev=" + ("" + str(std)) + serial.NEW_LINE)
+    serial.write_string("total time=" + ("" + str(total_time)) + " sec   time on=" + str(quad[Math.idiv(total_time, 240)]) + serial.NEW_LINE)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global forever_stop2
+    forever_stop2 = True
+    serial.write_string("" + serial.NEW_LINE + serial.NEW_LINE + "hourID, minsOn" + serial.NEW_LINE)
+    radio.send_string("hourID, minsOn")
+    i = 0
+    while i <= Math.idiv(total_time, 240) + 1 - 1:
+        s = "" + str(i) + ", " + ("" + str(quad[i])) + serial.NEW_LINE
+        serial.write_string(s)
+        radio.send_string(s)
+        basic.pause(100)
+        i += 1
+    serial.write_string("" + (serial.NEW_LINE))
+    serial.write_value("total_time", total_time)
+    forever_stop2 = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def setup():
+    global forever_stop3, mea, std, total_time, time_on, ll2
+    forever_stop3 = True
+    mea = 0
+    std = 0
+    total_time = 0
+    # total time counting
+    time_on = 0
+    j = 0
+    while j < 3000:
+        quad[j] = 0
+        j += 1
+    ll2 = get_data_point()
+    j = 0
+    while j < 6:
+        basic.show_number(5 - j)
+        j += 1
+    basic.clear_screen()
+    # input.light_level()
+    calc_stats(40, 200)
+    # calculate the statistics for light on. 8 seconds
+    if std < 1:
+        std = 1
+    serial.write_string("mean=" + ("" + str(mea)) + "  stdev=" + ("" + str(std)) + "\n")
+    basic.show_icon(IconNames.YES)
+    basic.pause(2000)
+    basic.clear_screen()
+    # input.light_level()
+    forever_stop3 = False
+# calculate statistics for a number of readings
+# returns mean, stdev - no long term storage needed
+# param: cnt - how many measurements to calculate
+# param: dly - milliseconds btw measurements
+# for the algo see https://en.wikipedia.org/wiki/Standard_deviation#Rapid_calculation_methods
+# number of items
+def calc_stats(cnt: number, dly: number):
+    global n, k, A, Q, mean, stdev, mea, std
+    # running k value. It's k=t+1 since k starts from 0.
+    n = cnt
+    t = 0
+    while t <= n - 1:
+        k = t + 1
+        Aprev = A
+        thedata = get_data_point()
+        # get next data point
+        A = A + (thedata - A) / k
+        Q = Q + (thedata - Aprev) * (thedata - A)
+        mean += thedata
+        basic.pause(dly)
+        t += 1
+    stdev = Math.sqrt(Q / (n - 1))
+    mean /= n
+    mea = mean
+    std = Math.ceil(stdev)
+stdev = 0
+Q = 0
+A = 0
+k = 0
+n = 0
+ll2 = 0
+time_on = 0
+forever_stop3 = False
+forever_stop2 = False
+std = 0
+mea = 0
+ll3 = 0
+total_time = 0
+mean = 0
+
 # Light state counter v1.00
 # We measure the time in seconds a light source is emitting light.
 # Learn (LS should be on):
 # User presses A and program calculates mean and standard deviation of the brightness emitted by the LS.
-# 
-
 radio.set_group(77)
 radio.set_transmit_power(7)
-
-forever_stop = False
-
-mea=0
-std=0
-total_time=0    #total time counting
-time_on=0       #time the light source is on
-
-#initialize hour count
-# total_time % 240 is the bin to add time_on.
-# it's good for 3000*4/1440 hours = 8 days
-quad=bytearray(3000)    #every quad represents 240 seconds.
-
+quad = bytearray(3000)
+# every quad represents 240 seconds.
 setup()
 
 def on_forever():
-    global total_time, time_on, forever_stop, quad
-    if not forever_stop:
+    global total_time
+    forever_stop = 0
+    if not (forever_stop):
         basic.pause(100)
-        led.unplot(1,1)
+        led.unplot(1, 1)
         basic.pause(900)
         ll = get_data_point()
-        total_time = total_time+1   # increase seconds
-        #if ll >= mea-3*std:      # If light level is above the mean - 3 stdev then consider it on
-            #time_on = time_on + 1
-        if ll >= 0.7 * mea:     # if light level is above 70% of mean
-            quad[total_time // 240] += 1    # increase the appropriate bin by 1 sec max secs per cell is 240=4min
-            led.plot(1,1)
-
+        total_time = total_time + 1
+        # increase seconds
+        # if ll >= mea-3*std:      # If light level is above the mean - 3 stdev then consider it on
+        # time_on = time_on + 1
+        if ll >= 0.7 * mea:
+            quad[Math.idiv(total_time, 240)] += 1
+            # increase the appropriate bin by 1 sec max secs per cell is 240=4min
+            led.plot(1, 1)
 basic.forever(on_forever)
-
-def on_button_pressed_a():
-    serial.write_string("mean light level="+str(mea)+"  stdev="+str(std)+serial.NEW_LINE)
-    serial.write_string("total time="+str(total_time)+" sec   time on="+quad[total_time // 240]+serial.NEW_LINE)
-
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_ab():
-    setup()
-
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_button_pressed_b():
-    global quad
-    forever_stop = True
-    serial.write_string(serial.NEW_LINE+serial.NEW_LINE+"hourID, minsOn"+serial.NEW_LINE)
-    radio.send_string("hourID, minsOn")
-    for i in range(total_time // 240 + 1):
-        s = str(i)+", "+str(quad[i])+serial.NEW_LINE
-        serial.write_string(s)
-        radio.send_string(s)
-        basic.pause(100)
-
-    serial.write_string(serial.NEW_LINE)
-    serial.write_value("total_time", total_time)
-    forever_stop = False
-
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def setup():
-    global mea, std, total_time, time_on, quad
-    forever_stop = True
-    mea=0
-    std=0
-    total_time=0    #total time counting
-    time_on=0       #time the light source is on
-    #hour=bytearray(240)
-    for i in range(3000):
-        quad[i]=0
-    ll=get_data_point()
-    #ll=get_data_point()
-    for i in range(6):
-        basic.show_number(5-i)
-    basic.clear_screen()
-    #input.light_level()
-    calc_stats(40, 200)    #calculate the statistics for light on. 8 seconds
-    if std < 1: std = 1
-    serial.write_string("mean="+str(mea)+"  stdev="+str(std)+"\n")
-    basic.show_icon(IconNames.YES)
-    basic.pause(2000)
-    basic.clear_screen()
-    #input.light_level()
-    forever_stop = False
-
-#calculate statistics for a number of readings
-#returns mean, stdev - no long term storage needed
-#param: cnt - how many measurements to calculate
-#param: dly - milliseconds btw measurements
-def calc_stats(cnt, dly):
-    global mea, std
-    # for the algo see https://en.wikipedia.org/wiki/Standard_deviation#Rapid_calculation_methods
-    A = 0
-    Q = 0
-    k = 0 #running k value. It's k=t+1 since k starts from 0.
-    n = cnt # number of items
-    mean = 0
-    for t in range(n):
-        k=t+1
-        Aprev = A
-        thedata=get_data_point()   #get next data point
-        A = A + (thedata-A)/k
-        Q = Q + (thedata-Aprev)*(thedata-A)
-        mean += thedata
-        basic.pause(dly)
-    stdev = Math.sqrt(Q/(n-1)) #sample standard deviation
-    mean /= n
-    mea = mean
-    std = Math.ceil(stdev)
-
-#to apply to a different sensor just change the input
-def get_data_point():
-    ll = input.light_level()
-    #print(str(ll))
-    return ll
--- main.ts
+++ main.ts
@@ -1,26 +1,132 @@
+// 
+//  Button A: print mean total time
+//  Button B: print statistics
+//  total time counting
+//  to apply to a different sensor just change the input
+function get_data_point(): number {
+    
+    ll3 = input.lightLevel()
+    //  print(str(ll))
+    return ll3
+}
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    setup()
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    serial.writeString("mean light level=" + ("" + ("" + mea)) + "  stdev=" + ("" + ("" + std)) + serial.NEW_LINE)
+    serial.writeString("total time=" + ("" + ("" + total_time)) + " sec   time on=" + ("" + quad[Math.idiv(total_time, 240)]) + serial.NEW_LINE)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let s: string;
+    
+    forever_stop2 = true
+    serial.writeString("" + serial.NEW_LINE + serial.NEW_LINE + "hourID, minsOn" + serial.NEW_LINE)
+    radio.sendString("hourID, minsOn")
+    let i = 0
+    while (i <= Math.idiv(total_time, 240) + 1 - 1) {
+        s = "" + ("" + i) + ", " + ("" + ("" + quad[i])) + serial.NEW_LINE
+        serial.writeString(s)
+        radio.sendString(s)
+        basic.pause(100)
+        i += 1
+    }
+    serial.writeString("" + serial.NEW_LINE)
+    serial.writeValue("total_time", total_time)
+    forever_stop2 = false
+})
+function setup() {
+    
+    forever_stop3 = true
+    mea = 0
+    std = 0
+    total_time = 0
+    //  total time counting
+    time_on = 0
+    let j = 0
+    while (j < 3000) {
+        quad[j] = 0
+        j += 1
+    }
+    ll2 = get_data_point()
+    j = 0
+    while (j < 6) {
+        basic.showNumber(5 - j)
+        j += 1
+    }
+    basic.clearScreen()
+    //  input.light_level()
+    calc_stats(40, 200)
+    //  calculate the statistics for light on. 8 seconds
+    if (std < 1) {
+        std = 1
+    }
+    
+    serial.writeString("mean=" + ("" + ("" + mea)) + "  stdev=" + ("" + ("" + std)) + "\n")
+    basic.showIcon(IconNames.Yes)
+    basic.pause(2000)
+    basic.clearScreen()
+    //  input.light_level()
+    forever_stop3 = false
+}
+
+//  calculate statistics for a number of readings
+//  returns mean, stdev - no long term storage needed
+//  param: cnt - how many measurements to calculate
+//  param: dly - milliseconds btw measurements
+//  for the algo see https://en.wikipedia.org/wiki/Standard_deviation#Rapid_calculation_methods
+//  number of items
+function calc_stats(cnt: number, dly: number) {
+    let Aprev: number;
+    let thedata: number;
+    
+    //  running k value. It's k=t+1 since k starts from 0.
+    n = cnt
+    let t = 0
+    while (t <= n - 1) {
+        k = t + 1
+        Aprev = A
+        thedata = get_data_point()
+        //  get next data point
+        A = A + (thedata - A) / k
+        Q = Q + (thedata - Aprev) * (thedata - A)
+        mean += thedata
+        basic.pause(dly)
+        t += 1
+    }
+    stdev = Math.sqrt(Q / (n - 1))
+    mean /= n
+    mea = mean
+    std = Math.ceil(stdev)
+}
+
+let stdev = 0
+let Q = 0
+let A = 0
+let k = 0
+let n = 0
+let ll2 = 0
+let time_on = 0
+let forever_stop3 = false
+let forever_stop2 = false
+let std = 0
+let mea = 0
+let ll3 = 0
+let total_time = 0
+let mean = 0
 //  Light state counter v1.00
 //  We measure the time in seconds a light source is emitting light.
 //  Learn (LS should be on):
 //  User presses A and program calculates mean and standard deviation of the brightness emitted by the LS.
-//  
 radio.setGroup(77)
 radio.setTransmitPower(7)
-let forever_stop = false
-let mea = 0
-let std = 0
-let total_time = 0
-// total time counting
-let time_on = 0
-// time the light source is on
-// initialize hour count
-//  total_time % 240 is the bin to add time_on.
-//  it's good for 3000*4/1440 hours = 8 days
 let quad = control.createBuffer(3000)
-// every quad represents 240 seconds.
+//  every quad represents 240 seconds.
 setup()
 basic.forever(function on_forever() {
     let ll: number;
     
+    let forever_stop = 0
     if (!forever_stop) {
         basic.pause(100)
         led.unplot(1, 1)
@@ -28,10 +134,9 @@ basic.forever(function on_forever() {
         ll = get_data_point()
         total_time = total_time + 1
         //  increase seconds
-        // if ll >= mea-3*std:      # If light level is above the mean - 3 stdev then consider it on
-        // time_on = time_on + 1
+        //  if ll >= mea-3*std:      # If light level is above the mean - 3 stdev then consider it on
+        //  time_on = time_on + 1
         if (ll >= 0.7 * mea) {
-            //  if light level is above 70% of mean
             quad[Math.idiv(total_time, 240)] += 1
             //  increase the appropriate bin by 1 sec max secs per cell is 240=4min
             led.plot(1, 1)
@@ -40,101 +145,3 @@ basic.forever(function on_forever() {
     }
     
 })
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    serial.writeString("mean light level=" + ("" + mea) + "  stdev=" + ("" + std) + serial.NEW_LINE)
-    serial.writeString("total time=" + ("" + total_time) + " sec   time on=" + quad[Math.idiv(total_time, 240)] + serial.NEW_LINE)
-})
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    setup()
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    let s: string;
-    
-    let forever_stop = true
-    serial.writeString(serial.NEW_LINE + serial.NEW_LINE + "hourID, minsOn" + serial.NEW_LINE)
-    radio.sendString("hourID, minsOn")
-    for (let i = 0; i < Math.idiv(total_time, 240) + 1; i++) {
-        s = "" + i + ", " + ("" + quad[i]) + serial.NEW_LINE
-        serial.writeString(s)
-        radio.sendString(s)
-        basic.pause(100)
-    }
-    serial.writeString(serial.NEW_LINE)
-    serial.writeValue("total_time", total_time)
-    forever_stop = false
-})
-function setup() {
-    let i: number;
-    
-    let forever_stop = true
-    mea = 0
-    std = 0
-    total_time = 0
-    // total time counting
-    time_on = 0
-    // time the light source is on
-    // hour=bytearray(240)
-    for (i = 0; i < 3000; i++) {
-        quad[i] = 0
-    }
-    let ll = get_data_point()
-    // ll=get_data_point()
-    for (i = 0; i < 6; i++) {
-        basic.showNumber(5 - i)
-    }
-    basic.clearScreen()
-    // input.light_level()
-    calc_stats(40, 200)
-    // calculate the statistics for light on. 8 seconds
-    if (std < 1) {
-        std = 1
-    }
-    
-    serial.writeString("mean=" + ("" + mea) + "  stdev=" + ("" + std) + "\n")
-    basic.showIcon(IconNames.Yes)
-    basic.pause(2000)
-    basic.clearScreen()
-    // input.light_level()
-    forever_stop = false
-}
-
-// calculate statistics for a number of readings
-// returns mean, stdev - no long term storage needed
-// param: cnt - how many measurements to calculate
-// param: dly - milliseconds btw measurements
-function calc_stats(cnt: number, dly: number) {
-    let Aprev: number;
-    let thedata: number;
-    
-    //  for the algo see https://en.wikipedia.org/wiki/Standard_deviation#Rapid_calculation_methods
-    let A = 0
-    let Q = 0
-    let k = 0
-    // running k value. It's k=t+1 since k starts from 0.
-    let n = cnt
-    //  number of items
-    let mean = 0
-    for (let t = 0; t < n; t++) {
-        k = t + 1
-        Aprev = A
-        thedata = get_data_point()
-        // get next data point
-        A = A + (thedata - A) / k
-        Q = Q + (thedata - Aprev) * (thedata - A)
-        mean += thedata
-        basic.pause(dly)
-    }
-    let stdev = Math.sqrt(Q / (n - 1))
-    // sample standard deviation
-    mean /= n
-    mea = mean
-    std = Math.ceil(stdev)
-}
-
-// to apply to a different sensor just change the input
-function get_data_point(): number {
-    let ll = input.lightLevel()
-    // print(str(ll))
-    return ll
-}
-
